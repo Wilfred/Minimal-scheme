45a4adf4a1653b280e50e6819fcc4758e9402c52
Wilfred Hughes <me@wilfred.me.uk>
2011-04-16 11:26:59 +0100

    give a more friendly output when displaying lists

--- main.py
+++ main.py
@@ -8,10 +8,26 @@ assert sys.version.startswith('3.'), "Python 3 required"
 
 from lexer import lexer
 from evaluator import eval_program, InterpreterException, load_standard_library
+from utils import flatten_linked_list
 
 class Repl(cmd.Cmd):
     intro = "Welcome to Minimal Scheme 0.1 alpha."
     prompt = "scheme> "
+
+    def get_external_representation(self, internal_representation):
+        if type(internal_representation) == tuple:
+            # list
+            external_representation = "("
+
+            for element in flatten_linked_list(internal_representation):
+                external_representation += self.get_external_representation(element) + ' '
+
+            external_representation = external_representation.rstrip() + ')'
+
+            return external_representation
+        else:
+            # atom
+            return str(internal_representation)
 
     def onecmd(self, program):
         if program == 'EOF':
@@ -22,7 +38,7 @@ class Repl(cmd.Cmd):
             result = eval_program(program)
 
             if not result is None:
-                print(result)
+                print(self.get_external_representation(result))
 
         except InterpreterException as e:
             print("Error: %s" % e.message)
