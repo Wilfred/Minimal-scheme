68ed78fa05efb970a60a0daf255f4fe22f1d79a9
Wilfred Hughes <me@wilfred.me.uk>
2011-04-16 09:07:26 +0100

    refactoring tests to use a assertEvaluatesTo method

--- tests.py
+++ tests.py
@@ -7,7 +7,15 @@ from evaluator import eval_program, variables, load_standard_library
 assert sys.version.startswith('3.'), "Python 3 required"
 
 
-class LexerText(unittest.TestCase):
+class InterpreterTest(unittest.TestCase):
+    def setUp(self):
+        load_standard_library()
+
+    def assertEvaluatesTo(self, program, expected_result):
+        result = eval_program(program)
+        self.assertEqual(result, expected_result)
+
+class LexerText(InterpreterTest):
     def test_integer(self):
         program = "3"
         result = eval_program(program)
@@ -23,81 +31,78 @@ class LexerText(unittest.TestCase):
         self.assertEqual(type(result), float)
 
 
-class InterpreterTest(unittest.TestCase):
+class EvaluatorTest(InterpreterTest):
     def test_variable_evaluation(self):
         program = "(define y 28) y"
 
-        self.assertEqual(eval_program(program), 28)
+        self.assertEvaluatesTo(program, 28)
 
     def test_procedure_call(self):
         program = "((if #f + *) 3 4)"
 
-        self.assertEqual(eval_program(program), 12)
+        self.assertEvaluatesTo(program, 12)
 
     def test_if_two_arguments(self):
         program = "(if #t 1)"
 
-        self.assertEqual(eval_program(program), 1)
+        self.assertEvaluatesTo(program, 1)
 
     def test_if_three_arguments(self):
         program = "(if #t 2 3)"
 
-        self.assertEqual(eval_program(program), 2)
+        self.assertEvaluatesTo(program, 2)
 
     def test_variable_assignment(self):
         program = "(define x 1) (set! x 2) x"
 
-        self.assertEqual(eval_program(program), 2)
+        self.assertEvaluatesTo(program, 2)
 
     def test_function_definition(self):
         program = "(define (z) 1) (z)"
 
-        self.assertEqual(eval_program(program), 1)
+        self.assertEvaluatesTo(program, 1)
 
     def test_variadic_function_definition(self):
         program = "(define (foo . args) 1) (foo)"
 
-        self.assertEqual(eval_program(program), 1)
+        self.assertEvaluatesTo(program, 1)
 
     def test_lambda(self):
         program = "((lambda (x) (+ x x)) 4)"
 
-        self.assertEqual(eval_program(program), 8)
+        self.assertEvaluatesTo(program, 8)
 
     def test_comment(self):
         program = "; 1"
 
-        self.assertEqual(eval_program(program), None)
+        self.assertEvaluatesTo(program, None)
 
-class MathsTest(unittest.TestCase):
-    def setUp(self):
-        load_standard_library()
-
+class MathsTest(InterpreterTest):
     def test_division(self):
         program = "(/ 8)"
-        self.assertEqual(eval_program(program), 0.125)
+        self.assertEvaluatesTo(program, 0.125)
 
         program = "(/ 12 3 2)"
-        self.assertEqual(eval_program(program), 2)
+        self.assertEvaluatesTo(program, 2)
 
     def test_less_than(self):
         program = "(< 1 1)"
-        self.assertEqual(eval_program(program), False)
+        self.assertEvaluatesTo(program, False)
 
         program = "(< 1 2 4)"
-        self.assertEqual(eval_program(program), True)
+        self.assertEvaluatesTo(program, True)
 
     def test_greater_than(self):
         program = "(> 1 1)"
-        self.assertEqual(eval_program(program), False)
+        self.assertEvaluatesTo(program, False)
 
         program = "(> 11 10 0)"
-        self.assertEqual(eval_program(program), True)
+        self.assertEvaluatesTo(program, True)
 
     def test_zero_predicate(self):
         program = "(zero? 0)"
 
-        self.assertEqual(eval_program(program), True)
+        self.assertEvaluatesTo(program, True)
 
 
 if __name__ == '__main__':
