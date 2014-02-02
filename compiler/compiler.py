f9d3dab48bc8c0cc773a19080ec5828e66c16acb
Wilfred Hughes <me@wilfred.me.uk>
2014-02-02 13:36:47 +0000

    Compile scheme integer literals.

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,13 +1,24 @@
-def compile_scm():
+def compile_literal(value):
+    return "movl	$%s, %%eax" % value
+
+
+def compile_scm(code):
+    asm = ""
+    if isinstance(code, int):
+        asm += compile_literal(code)
+    
     template = """	.text
 	.globl	entry_point
 entry_point:
-	movl	$26, %eax
+	%s
 	ret
-    """
+    """ % (asm,)
     return template
 
 
 if __name__ == '__main__':
+    # todo: lex and parse
+    program = 34
+    
     with open('scheme.s', 'w') as f:
-        f.write(compile_scm())
+        f.write(compile_scm(program))
